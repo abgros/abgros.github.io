ba26569bacfa457b4d51be579484e158bc37374f
abgros <{ID}+{username}@users.noreply.github.com>
2022-05-10 23:01:51 -0400

    Initial commit

---
+++ setup.js
@@ -0,0 +1,25 @@
+// Initialize window
+window.onload = ()=> {
+	canvas = document.getElementById('canvas');
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+	ctx = canvas.getContext('2d');
+	ctx.font = '20px Arial';
+	fps = 60
+	setInterval(drawSquare, 1000/fps);
+}
+
+// Bound number to a range
+function bound(number, min, max) {
+	return Math.min(Math.max(number, min), max);
+}
+
+// Randomly returns true or false
+function chance(probability) {
+	return Math.random() < probability;
+}
+
+// Choose a random item from an array
+function randomItem(array) {
+	return array[Math.floor(Math.random() * array.length)];
+}
---
+++ square.js
@@ -0,0 +1,42 @@
+// Handle keypresses
+keysHeld = {};
+window.addEventListener("keydown", function(event) {
+	keysHeld[event.code] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+	keysHeld[event.code] = false;
+});
+
+// Initialize variables
+xpos = 0;
+ypos = 0;
+squareSize = 100;
+squareColor = "black";
+squareColors = ["red", "orange", "green", "blue", "purple", "black"];
+speed = 20;
+
+function drawSquare() {
+	// Draw background
+	ctx.fillStyle = 'white';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	// Change square position
+	if (keysHeld['KeyW']) ypos -= speed;
+	if (keysHeld['KeyA']) xpos -= speed;
+	if (keysHeld['KeyS']) ypos += speed;
+	if (keysHeld['KeyD']) xpos += speed;
+
+	// Keep it within the edges
+	xpos = bound(xpos, 0, canvas.width - squareSize);
+	ypos = bound(ypos, 0, canvas.height - squareSize);
+
+	// Occurs average of once per second
+	if (chance(1/fps)) {
+		squareColor = randomItem(squareColors);
+	}
+
+	// Draw square
+	ctx.fillStyle = squareColor;
+	ctx.fillRect(xpos, ypos, squareSize, squareSize);
+}
